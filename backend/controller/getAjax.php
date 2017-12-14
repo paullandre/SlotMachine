c78fb83ee20360fc9fd99f988c1fe307a1b290d8
Paul Andre <erdan08@yahoo.com>
2017-12-14 15:59:29 +0800

    Update getAjax.php

--- backend/controller/getAjax.php
+++ backend/controller/getAjax.php
@@ -14,15 +14,31 @@ if(isset($_POST) != null)
 		if($data[$i]['machine'] == "machine1" || $data[$i]['machine'] == "machine2" || $data[$i]['machine'] == "machine3" 
 			|| $data[$i]['machine'] == "machine4" || $data[$i]['machine'] == "machine5")
 		{
-			array_push($group, array("group1" => array("machine" => $data[$i]['machine'], "active" => $data[$i]['active'])));
+			$group[] = array("machine" => $data[$i]['machine'], "active" => $data[$i]['active']);			
 		}
 	}
-		
 }
+
+$array = $group;
+$result = array();
+
+$first = $array[0];
+for($i=1; $i<count($array); $i++){
+	$result = array_intersect ($first, $array[$i]);
+	$first = $result;
+}
+die(print_r($result));
 
 for($i = 0; $i < count($group); $i++)
 {
-	print $group[$i]['group1']['active'] . "\n";
+	$last = '';
+	print $last . "\n";
+	//print $group[$i]['group1']['active'] . "\n";
+	$first = $group[$i]['group1']['active'];
+	
+	$last = $first;
+	
+	print $first . "\n";		
 }
 //die(print_r($group));
 
